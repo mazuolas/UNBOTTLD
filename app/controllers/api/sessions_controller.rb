329a59032f52d07975e2001aecfbcdcae8e888e6
mark azuolas <markazuolas@gmail.com>
2017-05-15 11:46:29 -0700

    create session api util

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,23 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(user_params[:username],
+                                     user_params[:password])
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: ["Invalid login credentials"], status: 422
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout!
+      render 'api/users/show'
+    else
+      render json: ["No one signed in"], status: 404
+    end
+  end
 end
