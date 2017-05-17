e2cf3fc40c65b7f65c1bdb0724ebf3ee0d0d14d9
mark azuolas <markazuolas@gmail.com>
2017-05-17 14:23:55 -0700

    create drink index container

---
+++ frontend/components/drinks/drink_index_container.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import DrinkIndex from './drink_index';
+import { getAllDrinks } from '../../actions/drink_actions';
+
+const mapStateToProps = (state) => ({
+  drinks: state.drinks
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getAllDrinks: dispatch(getAllDrinks())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DrinkIndex)
