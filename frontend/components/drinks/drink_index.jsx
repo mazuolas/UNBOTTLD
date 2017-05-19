b0745beab6658d3e41e2f9205d24c81959b64c3c
mark azuolas <markazuolas@gmail.com>
2017-05-18 20:22:05 -0700

    style drink list

--- frontend/components/drinks/drink_index.jsx
+++ frontend/components/drinks/drink_index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class DrinkIndex extends React.Component {
   constructor(props){
@@ -13,6 +14,10 @@ class DrinkIndex extends React.Component {
     const drinks = this.props.drinks
     return(
       <ul className='drink-index'>
+        <li className='create-text'>
+          <h2>Can't find a bottle?</h2>
+          <Link className='create-link' to='/bottles/create'>Create it!</Link>
+        </li>
         {Object.keys(drinks).map((key) =>{
           if (key === 'errors') { return }
           return (
@@ -21,6 +26,7 @@ class DrinkIndex extends React.Component {
               <h1 className='drink-name'>{ drinks[key].name }</h1>
               <h2 className='brewery'>Brewery: { drinks[key].brewery_id }</h2>
               <button className='check-in'>Check In</button>
+              <Link className='edit-link' to={`/bottles/${key}/edit`}>Edit</Link>
               <div className='beer-details'>
                 <h3 className='detail abv'>{ drinks[key].abv } ABV</h3>
                 <h3 className='detail checkins'>'TODO' Checkins </h3>
