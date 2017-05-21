98f72129463bf4caa1400100af15c9bbba3b26a1
mark azuolas <markazuolas@gmail.com>
2017-05-20 17:15:57 -0700

    refactor drink page

--- frontend/components/drinks/drink_page.jsx
+++ frontend/components/drinks/drink_page.jsx
@@ -4,32 +4,41 @@ import { Link } from 'react-router-dom';
 class DrinkPage extends React.Component{
   constructor(props){
     super(props);
+    this.checkin = this.checkin.bind(this);
   }
 
   componentWillMount(){
     this.props.getDrink();
   }
 
+  checkin(key) {
+    return (e) =>{
+      e.preventDefault();
+      this.props.history.push(`/bottles/${key}/checkin`)
+    }
+  }
+
   render(){
-    if (!this.props.drink) {
+    const drink = this.props.drink
+    if (!drink) {
       return null
     }
     return (
       <div className='drink-page'>
         <div className='drink-index-item '>
-          <img className='drink-icon' src={this.props.drink.image_url} />
-          <h1 className='drink-name'>{ this.props.drink.name }</h1>
-          <h2 className='brewery'>Brewery: { this.props.drink.brewery }</h2>
-          <button className='check-in'>Check In</button>
-          <Link className='edit-link' to={`/bottles/${this.props.drink.id}/edit`}>Edit</Link>
+          <img className='drink-icon' src={drink.image_url} />
+          <h1 className='drink-name'>{ drink.name }</h1>
+          <h2 className='brewery'>Brewery: { drink.brewery }</h2>
+          <button className='check-in' onClick={this.checkin(drink.id)}>Check In</button>
+          <Link className='edit-link' to={`/bottles/${drink.id}/edit`}>Edit</Link>
           <div className='beer-details'>
-            <h3 className='detail abv'>{ this.props.drink.abv } ABV</h3>
+            <h3 className='detail abv'>{ drink.abv } ABV</h3>
             <h3 className='detail checkins'>'TODO' Checkins </h3>
-            <h3 className='detail rating'>Average Rating: { this.props.drink.average_rating }</h3>
-            <h3 className='detail date'>Added {this.props.drink.created_at}</h3>
+            <h3 className='detail rating'>Average Rating: { drink.average_rating }</h3>
+            <h3 className='detail date'>Added {drink.created_at}</h3>
           </div>
         </div>
-        <h3 className='drink-description'>{ this.props.drink.description }</h3>
+        <h3 className='drink-description'>{ drink.description }</h3>
         <h1 className='create-text'>Recent Checkins</h1>
         <div className='checkin-feed'>
           <h2 className="feed-item">Feed not built yet :(</h2>
