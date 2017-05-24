5651148bea2f029756f9251f26e208e564599f87
mark azuolas <markazuolas@gmail.com>
2017-05-24 15:51:41 -0700

    user smaller splash page image

--- frontend/components/index.jsx
+++ frontend/components/index.jsx
@@ -10,7 +10,7 @@ class Index extends React.Component {
 
     return (
       <div className='index'>
-        <img src='https://res.cloudinary.com/dcslgdwha/image/upload/c_scale,q_80,w_2000/v1495148251/bottles_d4suts.jpg' className='background-image'/>
+        <img src='https://res.cloudinary.com/dcslgdwha/image/upload/v1495666085/bottles_d4suts_hbjvz9.jpg' className='background-image'/>
         <h1 className='page-title'><img className='icon' src='https://res.cloudinary.com/dcslgdwha/image/upload/v1495151026/white_bottle_dglppo.png'/>UNBOTTLD</h1>
         <p className='description'>Find your favorite bottle</p>
         <div className='link-container'>
